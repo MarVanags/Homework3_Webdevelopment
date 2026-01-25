6bf0c461e3f4996d322b03db3b2278ec8391d467
Mārtiņš Vanags <martins.vanags02@gmail.com>
2026-01-25 11:09:50 +0200

    Checking if divs display properly

--- Scripts3/Hwscript3.js
+++ Scripts3/Hwscript3.js
@@ -1,1 +1,42 @@
 console.log("Work in progress")
+
+// Setting the necessary values
+
+const numbersdivs = document.getElementById("numberspan");
+
+// Creating a new div
+
+for (let numbercounter = 1; numbercounter < 101; numbercounter++) {
+
+    let nextdiv = document.createElement("div");
+    nextdiv.id = "numberdiv" + numbercounter;
+
+    if (numbercounter % 3 !== 0 && numbercounter % 5 !== 0) {
+        nextdiv.className = "neither";
+        nextdiv.innerHTML += "<p>Number: " + numbercounter + " (none!)</p>";
+        numbersdivs.appendChild(nextdiv);
+    }
+
+    else if (numbercounter % 3 === 0 && numbercounter % 5 !== 0) {
+        nextdiv.className = "fizz";
+        nextdiv.innerHTML += "<p>Number: " + numbercounter + " (fizz!)</p>";
+        numbersdivs.appendChild(nextdiv);
+    }
+
+    else if (numbercounter % 3 !== 0 && numbercounter % 5 === 0) {
+        nextdiv.className = "buzz" ;
+        nextdiv.innerHTML += "<p>Number: " + numbercounter + " (buzz!)</p>";
+        numbersdivs.appendChild(nextdiv);
+    }
+
+    else if (numbercounter % 3 === 0 && numbercounter % 5 === 0) {
+        nextdiv.className = "both";
+        nextdiv.innerHTML += "<p>Number: " + numbercounter + " (fizzbuzz!)</p>";
+        numbersdivs.appendChild(nextdiv);
+    }
+
+    else {
+        nextdiv.style.display = "none";
+    }
+    
+}
